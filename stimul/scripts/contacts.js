54171acda5f25e6afbdbbed786e3140c8bab48cc
shinjitsu-1991 <shinjitsu1991@gmail.com>
2018-01-04 18:27:36 +0200

    v.1.2.1

---
+++ stimul/scripts/contacts.js
@@ -0,0 +1,27 @@
+//оплата онлайн форма
+
+$('#payment').click(function(){
+    $('.online-pay-form').css({'display':'flex'});
+});
+
+$('#online-close').click(function(){
+    $('.online-pay-form').css({'display':'none'});
+});
+
+ //выпадающее меню в адаптивной версии
+
+ $('#burger-adaptiv').click(function(){
+    $('#burger-adaptiv').hide(100);
+    $('#close-burger-adaptiv').show(100);
+    $('.drop-menu').show(500);
+    $('.head-baner__main').css({'height':'600px'});
+    $('.head-baner__main').css({'visibility':'hidden'});
+ });
+
+ $('#close-burger-adaptiv').click(function(){
+    $('#close-burger-adaptiv').hide(100);
+    $('#burger-adaptiv').show(200);
+    $('.drop-menu').hide(300);
+    $('.head-baner__main').css({'height':'auto'});
+    $('.head-baner__main').css({'visibility':'visible'});
+ });
