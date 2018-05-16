929cc109a84f882f745bf4621b6b81368b9f8248
shinjitsu-1991 <shinjitsu1991@gmail.com>
2018-05-16 04:05:52 +0200

    2

--- THE_RAVEN/js/api.js
+++ THE_RAVEN/js/api.js
@@ -18,3 +18,30 @@ var swiper = new Swiper('.swiper-container', {
   },
     });
 
+$(document).ready(function(){
+
+    $('.prev-page .logo').css({opacity:1});
+    $('.prev-page .logo').animate({opacity:0},2000);
+    $('.prev-page .logo').animate({opacity:1},2000);
+    setTimeout(arguments.callee, 4500);
+    setTimeout(function () {
+        $('.prev-page .menu #item-1').fadeIn(100);
+    },4000);
+    setTimeout(function () {
+        $('.prev-page .menu #item-2').fadeIn(100);
+    },4400);
+    setTimeout(function () {
+        $('.prev-page .menu #item-3').fadeIn(100);
+    },4800);
+    setTimeout(function () {
+        $('.prev-page .menu #item-4').fadeIn(100);
+    },5200);
+
+});
+$('.menu-item .attr').css({opacity:0});
+$('.menu-item').mouseenter(function(){
+    $(this).children('.attr').animate({opacity:1},500);
+});
+$('.menu-item').mouseleave(function(){
+    $(this).children('.attr').animate({opacity:0},300);
+});
